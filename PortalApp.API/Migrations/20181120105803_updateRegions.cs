e5dfcc8f0452302053c352d2ecbd7fc9053807a5
Eldar Absalyamov <salyamkin@gmail.com>
2018-11-26 16:18:06 +0600

    first commit

---
+++ PortalApp.API/Migrations/20181120105803_updateRegions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortalApp.API.Migrations
+{
+    public partial class updateRegions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Regions",
+                newName: "NameRu");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameEn",
+                table: "Regions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameKz",
+                table: "Regions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NameEn",
+                table: "Regions");
+
+            migrationBuilder.DropColumn(
+                name: "NameKz",
+                table: "Regions");
+
+            migrationBuilder.RenameColumn(
+                name: "NameRu",
+                table: "Regions",
+                newName: "Name");
+        }
+    }
+}
